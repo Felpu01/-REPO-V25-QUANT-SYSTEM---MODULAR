f306f4cb402d7a90c715e6e5303894f6d95f8a81
Felpu01 <matiasgonzalezleonel@icloud.com>
2026-05-12 07:01:54 -0300

    Update market_data.py

--- data/market_data.py
+++ data/market_data.py
@@ -1,1 +1,15 @@
+import random
 
+def get_market_data():
+
+    data = []
+    price = 10000
+
+    for _ in range(300):
+        price += random.uniform(-20, 20)
+
+        data.append({
+            "price": price
+        })
+
+    return data
