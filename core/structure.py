e02485bfb9c6701b88d354d91c593f9615205ce7
Felpu01 <matiasgonzalezleonel@icloud.com>
2026-05-12 07:03:01 -0300

    Update structure.py

--- core/structure.py
+++ core/structure.py
@@ -1,1 +1,14 @@
+def bos(price, prev_high, prev_low):
 
+    return price > prev_high, price < prev_low
+
+
+def choch(trend, bos_up, bos_down):
+
+    if trend == "DOWN" and bos_up:
+        return True, False
+
+    if trend == "UP" and bos_down:
+        return False, True
+
+    return False, False
