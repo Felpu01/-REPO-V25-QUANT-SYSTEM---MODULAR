6152a70b2c7f6d536e2b4498adb1d25a4641d203
Felpu01 <matiasgonzalezleonel@icloud.com>
2026-05-12 02:38:08 -0300

    Create strategy.py

---
+++ strategy.py
@@ -0,0 +1,36 @@
+# strategy.py
+
+def institutional_score(trend, volatility, sweep, regime):
+
+    score = 0
+
+    # TREND
+    if trend in ["TREND_UP", "TREND_DOWN"]:
+        score += 0.25
+
+    # VOLATILITY
+    if volatility == "HIGH":
+        score += 0.25
+
+    elif volatility == "MEDIUM":
+        score += 0.15
+
+    # LIQUIDITY SWEEP
+    if sweep:
+        score += 0.35
+
+    # REGIME FILTER
+    if regime == "TRENDING":
+        score += 0.15
+
+    elif regime == "RANGING":
+        score -= 0.10
+
+    # LIMITS
+    if score > 1:
+        score = 1.0
+
+    if score < 0:
+        score = 0
+
+    return round(score, 2)
